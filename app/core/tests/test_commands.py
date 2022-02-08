d5bac99df7ddb42f12e014d00ea60dc391ec0ca8
--unset <shreyakaushik11@gmail.com>
2022-02-08 19:54:37 +0530

    Added wait_for_db command and unit test for it

---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,27 @@
+from unittest.mock import patch
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import TestCase
+
+
+class CommandTests(TestCase):
+
+    def test_wait_for_db_ready(self):
+        """Test waiting for db when db is available"""
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
+            gi.return_value = True
+            call_command('wait_for_db')
+            """We're going to check that the getitem function was called once"""
+            self.assertEqual(gi.call_count, 1)
+
+    @patch('time.sleep', return_value=True)
+    def test_wait_for_db(self, ts):
+        """Test waiting for db"""
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
+            """the first 5 times you call the getitem, it's going to
+            raise the OperationalError and then on the 6th time, it
+            won't raise the error, it will just return"""
+            gi.side_effect = [OperationalError] * 5 + [True]
+            call_command('wait_for_db')
+            self.assertEqual(gi.call_count, 6)
